7cdd3979bd4fbc4e4a2b50fa15fca16f58e80e3f
jonyb <jonybilstein@gmail.com>
2023-10-23 12:05:48 -0400

    Add project files.

---
+++ MyMutualFund/Model/Fund.cs
@@ -0,0 +1,50 @@
+﻿using MyMutualFind.Interfaces;
+
+
+
+namespace MyMutualFind.Model
+{
+    public class Fund
+    {
+        public string TickerSymbol { get; }
+        decimal CashAvailable { get; set }
+        int QtyOfShares { get; }
+        decimal PricePerShare { get; }
+
+        List<Share> PortFolio { get; }
+
+        IStockExchangeCenter _StockExchangeCenter;
+
+        public Fund(string tickerSymbol, int initialQtyOfShares, IStockExchangeCenter stockExchangeCenter)
+        {
+            TickerSymbol = tickerSymbol;
+            PortFolio = new List<Share>();
+            this.QtyOfShares = initialQtyOfShares;
+            this._StockExchangeCenter = stockExchangeCenter;
+        }
+
+        public (bool, Share) Buy(string symbol)
+        {
+
+            _StockExchangeCenter.PeekShare(symbol);
+
+            if (CashAvailable <= CashAvailable) return (false, null);
+
+            var shareBought = _StockExchangeCenter.Sell(symbol);
+            if (shareBought != null)
+            {
+                PortFolio.Add(shareBought);
+                CashAvailable -= shareBought.SharePrice;
+            }
+
+            return (true, shareBought);
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ MyMutualFund/Model/Share.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMutualFind.Model
+{
+    public class Share
+    {
+        string ShareId { get;  }
+        string Symbol { get;  }
+        public decimal SharePrice { get;  }
+
+
+    }
+}
